6e8ef381d4a3bb790f60c6da786654d3507fc65b
Juan David Vega N <juan.vega01@uceva.edu.co>
2022-04-03 20:43:41 -0500

    Se hizo una impresion del Hola mundo

--- src/ejemplogit/Ejemplogit.java
+++ src/ejemplogit/Ejemplogit.java
@@ -12,6 +12,6 @@ public class Ejemplogit{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
    }
 }
