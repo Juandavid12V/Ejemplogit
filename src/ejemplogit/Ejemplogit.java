afde4607c580902c332529ea1be3b674096302c4
Juan David Vega N <juan.vega01@uceva.edu.co>
2022-04-03 20:37:49 -0500

    Se hizo una modificacion en la clase Ejemplogit

--- src/ejemplogit/Ejemplogit.java
+++ src/ejemplogit/Ejemplogit.java
@@ -2,19 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejemplogit;
-
+package Ejemplogit;
 /**
- *
+ * Esta clase es un ejemplo
  * @author Juan David Vega N
  */
-public class Ejemplogit {
-
+public class Ejemplogit{
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+   }
 }
